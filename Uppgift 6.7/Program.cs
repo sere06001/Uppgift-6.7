1138e76181a7fcd42e13ce5c93859315c8fbc07a
sere06001 <sere06001@utb.vaxjo.se>
2024-02-15 15:03:31 +0100

    Add project files.

---
+++ Uppgift 6.7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace uppgift6_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Skriv ett heltal: ");
+                int heltal = int.Parse(Console.ReadLine());
+                Primtal(heltal);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        static void Primtal(int a)
+        {
+            bool prim = true;
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    prim = false;
+                    break;
+                }
+            }
+            if (prim)
+            {
+                Console.WriteLine(a+" är ett primtal.");
+            }
+            else
+            {
+                Console.WriteLine(a+" är inte ett primtal.");
+            }
+        }
+    }
+}
